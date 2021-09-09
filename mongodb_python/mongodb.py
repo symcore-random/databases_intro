0ee6fbd315f1a536f97c6298fd9d4757e6e6299b
SchmitzErnany <ernany0@gmail.com>
2021-09-08 21:12:42 -0300

    mongodb script creation and sql script update

---
+++ mongodb_python/mongodb.py
@@ -0,0 +1,55 @@
+#%%
+from pymongo import MongoClient
+
+# connecting to the cluster already installed
+# in the machine or in the cloud server.
+# For installation of the mongodb service, 
+# check out the following links:
+# https://www.youtube.com/watch?v=yB5EWH5_XmA&ab_channel=TerminalRoot
+# https://terminalroot.com.br/2020/02/mongodb.html
+# For Ubuntu 20.04 users, follow the installation described in the website,
+# however pay attention to the creation and permissions of the database /data/db folder,
+# because otherwise the mongodb service cannot start very well. The instructions for 
+# the creation and permissions of this folder are at the gentoo part right above
+# the Ubuntu 20.04 installation steps.
+cluster = MongoClient("localhost:27017")
+# locking in to the database
+# if it is not already created,
+# is is created automatically
+database = cluster["mydatabase"]
+# locking in to the collection
+# if it is not already created,
+# it is created automatically
+collection = database["mycollection"]
+
+###########################################
+### inserting documents into collection ###
+###########################################
+
+# defining documents (no schema needed)
+document1 = {"_id": 13, "name": "myname"}
+document2 = {"name": "myname", "address": "New Orleans"}
+document3 = {"whatever": 5000, "disgusting": True}
+# insert many documents
+collection.insert_many([document1, document2])
+# insert one document
+collection.insert_one(document3)
+
+############################################
+### retrieving documents from collection ###
+############################################
+
+# find documents according to some key
+results_object = collection.find({"name":"myname"})
+results_list = list(results_object)
+# find all documents in a collection
+results_object = collection.find({})
+results_list = list(results_object)
+
+##########################
+### additional remarks ###
+##########################
+
+# One can also delete, update, count documents, etc.
+# However this little introduction might be sufficient
+# for general purposes.
--- sql_python/sql.py
+++ sql_python/sql.py
@@ -1,12 +1,15 @@
 import sqlite3
 
+# setting up the name of the database
+database_file = "words_database.db"
+
 #############################################
 ### creating a new sql database and table ###
 #############################################
 
-database_file = "words_database.db"
-# connecting to the database (this creates the database)
-conn = sqlite3.connect("word_lemma_postag.db")
+# connecting to the database (this creates the database if
+# it is not already created)
+conn = sqlite3.connect(database_file)
 # cursor that intermediates all operation in the database
 c = conn.cursor()
 # create table name 'words' with three columns 'w0', 'w1' and 'w2' of type 'text'
@@ -14,14 +17,14 @@ c.execute("CREATE TABLE words (w0 text, w1 text, w2 text)")
 # create an index on the first column for faster retrieval time (indexing in turn
 # makes the database larger) when executing a search on 'w0'
 c.execute("CREATE INDEX ix_words_w0 ON words (w0)")
-# close connection
+# commit and close connection
+conn.commit()
 conn.close()
 
 ###########################################
 ### inserting new rows to created table ###
 ###########################################
 
-database_file = "words_database.db"
 # connecting to the database
 conn = sqlite3.connect(database_file)
 # cursor that intermediates all operation in the database
@@ -31,14 +34,14 @@ word0 = 'ele'
 word1 = 'ameixa'
 word2 = 'cadeira'
 c.execute("INSERT INTO words VALUES (?, ?, ?)", (word0, word1, word2))
-# close connection
+# commit and close connection
+conn.commit()
 conn.close()
 
 #######################################
 ### retrieve information from table ###
 #######################################
 
-database_file = "words_database.db"
 # connecting to the database
 conn = sqlite3.connect(database_file)
 # cursor that intermediates all operation in the database
@@ -49,5 +52,14 @@ c.execute('SELECT * FROM words WHERE w0="{0}"'.format(word))
 # retrieve all results. Also, one could use the method '.fetchone()',
 # which would retrieve only the first row corresponding to the search
 retrieved_tuple = c.fetchall() 
-# close connection
+# commit and close connection
+conn.commit()
 conn.close()
+
+##########################
+### additional remarks ###
+##########################
+
+# One can also delete, update, count rows, etc.
+# However this little introduction might be sufficient
+# for general purposes.
